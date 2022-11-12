ead49b92f4d80b45be99b44b76a336e403efad45
stefano-chen <stefanochen01@gmail.com>
2022-11-12 13:36:14 +0100

    csv

--- sum_csv.py
+++ sum_csv.py
@@ -1,20 +1,15 @@
-import os
-
-
 def sum_csv(file_name):
     result = 0.0
+    empty = True
     file = open(file_name, "r")
     if file is not None:
-        if os.stat(file_name).st_size > 0:
-            for line in file:
-                element = line.split(',')
-                if element[0] != "Date":
-                    try:
-                        result += float(element[1])
-                    except:
-                        result += 0
-        else:
-            file.close()
-            return None
+        for line in file:
+            empty = False
+            element = line.split(',')
+            if element[0] != "Date":
+                try:
+                    result += float(element[1])
+                except:
+                    result += 0
     file.close()
-    return result
+    return result if not empty else None
